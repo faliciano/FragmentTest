398e101089c2aa5d157afd5d455cb21e0531f5f5
faliciano <56785572+faliciano@users.noreply.github.com>
2019-11-02 18:39:54 +0800

    Add Message transfer

--- app/src/main/java/us/syh/fragmenttest/DynamicFragment.java
+++ app/src/main/java/us/syh/fragmenttest/DynamicFragment.java
@@ -10,9 +10,19 @@ import androidx.fragment.app.Fragment;
 public class DynamicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        //在fragment中拿到Bundle对象
+        Bundle bundle=getArguments();
         //定义一个view对象，并返回
         //第一个参数为上面定义的布局文件，第二参数为传入的参数，第三个参数一般设为false
         View view=inflater.inflate(R.layout.dynamic_fragment,container,false);
         return view;
+    }
+    //绑定字符串到fragment a
+    public static DynamicFragment bundleStringToFragment(String message){
+        DynamicFragment fragment=new DynamicFragment();
+        Bundle bundle=new Bundle();
+        bundle.putString("message",message);
+        fragment.setArguments(bundle);
+        return fragment;
     }
 }
