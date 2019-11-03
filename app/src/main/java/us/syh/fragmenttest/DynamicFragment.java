f1f4509fc151ccbafccb0957f1f92932e479a75c
faliciano <56785572+faliciano@users.noreply.github.com>
2019-11-03 15:39:46 +0800

    Update android.yml
    Signed-off-by: faliciano <56785572+faliciano@users.noreply.github.com>

--- app/src/main/java/us/syh/fragmenttest/DynamicFragment.java
+++ app/src/main/java/us/syh/fragmenttest/DynamicFragment.java
@@ -4,29 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class DynamicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        //在fragment中拿到Bundle对象
-        Bundle bundle=getArguments();
-        if(bundle!=null){
-            String message=bundle.getString("message");
-            System.out.println(message);
-        }
         //定义一个view对象，并返回
         //第一个参数为上面定义的布局文件，第二参数为传入的参数，第三个参数一般设为false
         View view=inflater.inflate(R.layout.dynamic_fragment,container,false);
+        //在fragment中拿到Bundle对象
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            final String message = bundle.getString("message");
+            TextView textView = view.findViewById(R.id.textView);
+            LinearLayout linearlayout = view.findViewById(R.id.linearlayout);
+            Button but = new Button(view.getContext());
+            but.setText("传递值到Acitivity");
+            but.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getArguments().putString("message", "我来自Fragmernt哦！接收到的消息：(" + message + ")");
+                }
+            });
+            linearlayout.addView(but);
+            textView.setText(message);
+        }
         return view;
-    }
-    //绑定字符串到fragment a
-    public static DynamicFragment bundleStringToFragment(String message){
-        DynamicFragment fragment=new DynamicFragment();
-        Bundle bundle=new Bundle();
-        bundle.putString("message",message);
-        fragment.setArguments(bundle);
-        return fragment;
     }
 }
--- app/src/main/java/us/syh/fragmenttest/DynamicFragmentRun.java
+++ app/src/main/java/us/syh/fragmenttest/DynamicFragmentRun.java
@@ -2,11 +2,14 @@ package us.syh.fragmenttest;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DynamicFragmentRun extends AppCompatActivity {
     private FragmentManager manager;
@@ -20,11 +23,11 @@ public class DynamicFragmentRun extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 myfragment=new DynamicFragment();
-                //在activity中绑定数据到fragment:开始
+                //activity传值到fragment:开始
                 Bundle bundle=new Bundle();
                 bundle.putString("message","Hello,I am from fragment A!");
                 myfragment.setArguments(bundle);
-                //在activity中绑定数据到fragment:结束
+                //activity传值到fragment:结束
                 FragmentTransaction transaction=manager.beginTransaction();
                 transaction.add(R.id.fragments,myfragment);
                 transaction.commit();
@@ -33,7 +36,25 @@ public class DynamicFragmentRun extends AppCompatActivity {
         findViewById(R.id.button_sent_message).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DynamicFragment fragment=myfragment.bundleStringToFragment("HHHHHHHH!!!!    ");
+                SimpleDateFormat sdf = new SimpleDateFormat();// 格式化时间
+                sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");// a为am/pm的标记
+                Date date = new Date();// 获取当前时间
+                //activity传值到fragment
+                myfragment = new DynamicFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("message", "Hello,Fragment!\n时间:" + sdf.format(date));
+                myfragment.setArguments(bundle);
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.add(R.id.fragments, myfragment);
+                transaction.commit();
+            }
+        });
+        findViewById(R.id.button_receive_message).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = myfragment.getArguments();
+                String message = bundle.getString("message");
+                Toast.makeText(DynamicFragmentRun.this, message, Toast.LENGTH_LONG).show();
             }
         });
     }
